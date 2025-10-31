93b26c117f0515cc70461c7dc34ce2c18ee18578
nikhilrajput15 <nr580451@gmail.com>
2025-10-31 15:15:06 +0530

    commit done

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Bookmark } from 'lucide-react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='parent'>
+      <div className='card'>
+        <div className='top'>
+          <img src='https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg' alt='QR Code' />
+          <button>Save<Bookmark /></button>
+        </div>
+
+        <div className='center'>
+          <h3>Amazon <span>5 days ago</span></h3>
+          <h2>Senior Frontend Developer</h2>
+          <div>
+            <h4>Part Time</h4>
+            <h4>Senior Level</h4>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div>
+            <div>
+              <h3>$120/hr</h3>
+              <p>Mumbai, India</p>
+            </div>
+            <button>Apply Now</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
